ff4f76b86176dfe13f51124103b60274b3983f29
Cyberstizz <charles.lamb.dev@gmail.com>
2020-06-29 00:15:17 -0400

    everything

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     this is a predator test!
+    </div>
+  );
+}
+
+export default App;
